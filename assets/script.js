2bf6258a41a8b322e7e5cf5bf1e063116d953db3
Jeff Benak <jeff.benak@yahoo.com>
2021-09-07 20:42:10 -0500

    Added variables

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var queryURL = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=11ae9b19ddc64a1c99d8d93d7f304871&mapid=40380";
+
+var welcome = document.getElementById('welcome');
+var goButton = document.getElementById('lets-go');
+
+
+
+window.onload = function() {
+
+}
+// fetch (queryURL) {
+    
+// }
