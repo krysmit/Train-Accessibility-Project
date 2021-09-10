594b87fa1a6f400c05c0def4d7b07d6362ed9d5d
Jeff Benak <jeff.benak@yahoo.com>
2021-09-10 14:26:34 -0500

    Added customer alerts api and some vars

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,42 @@
-var queryURL = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=11ae9b19ddc64a1c99d8d93d7f304871&mapid=40380";
+// <-------------------------------Geo Location Script--------------------------------->
 
-var welcomeTitle = document.getElementById('welcome');
-var goButton = document.getElementById('lets-go');
-var permText = document.getElementById('perm');
-var noButton = document.getElementById('nobtn');
-var yesButton = document.getElementById('yesbtn');
-var h3Text = document.getElementById('h3text');
-var subButton = document.getElementById('subbtn');
-var subButton = document.getElementById('zip');
-var nearInfo = document.getElementById('nearinfo');
-var destInfo = document.getElementById('destinfo');
+// const successCallback = (position) => {
+//     console.log(position)
+// };
+
+// const errorCallback = (errorCallback) => {
+//     console.log(position)
+// };
+
+// navigator.geolocation.getCurrentPosition(successCallback, errorCallback, {
+//     enableHighAccuracy: true,
+//     timeout: 5000, //it is in milliseconds
+// });
+
+// watchId allows to keep updating the USER's location as they walk around
+// const watchId = navigator.geolocation.watchPosition(successCallback, errorCallback);
+
+// To stop watching location use the one below
+// navigator.geolocation.clearWatch(watchId);
+
+// <---------------------------------Another Option------------------------------->
+// This option is connected to a button on the HTML side.
+// Reference this code on mozzila @ https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API/Using_the_Geolocation_API#examples
+// Hopefully one of these works.
+
+
+
+var welcomeTitle = $('#welcome');
+var goButton = $('#lets-go');
+var permText = $('#perm');
+var noButton = $('#nobtn');
+var yesButton = $('#yesbtn');
+var h3Text = $('#h3text');
+var submitButton = $('#subbtn');
+var getZip = $('input[name="zipcode"]').val();
+var nearInfo = $('#nearinfo');
+var destInfo = $('#destinfo');
+
 
 
 
@@ -26,44 +53,79 @@ window.onload = function() {
     document.getElementById('destinfo').style.display = 'none';
 
 
+}
 
+
+
+function geoFindMe() {
+
+  const status = document.querySelector('#status');
+  const mapLink = document.querySelector('#map-link');
+
+  mapLink.href = '';
+  mapLink.textContent = '';
+
+  function success(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    status.textContent = '';
+    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+  }
+
+  function error() {
+    status.textContent = 'Unable to retrieve your location';
+  }
+
+  if(!navigator.geolocation) {
+    status.textContent = 'Geolocation is not supported by your browser';
+  } else {
+    status.textContent = 'Locating…';
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
 
 }
 
-    function geoFindMe() {
-        if (!navigator.geolocation){
-         console.log("Geolocation is not supported by your browser");
-          return;
+document.querySelector('#find-me').addEventListener('click', geoFindMe);
+
+
+
+      function subButton(event) {
+        event.preventDefault();
+  
+        var getZip = $('input[name="zipcode"]').val();
+        console.log(getZip);
+  
+        if (!getZip) {
+            return;
         }
-        function success(position) {
-          var latitude  = position.coords.latitude; 
-          var longitude = position.coords.longitude;
-          reverseGeocodingWithGoogle(longitude, latitude);
-          console.log(reverseGeocodingWithGoogle(longitude, latitude));
-        }
-        function error() {
-          console.log("Unable to retrieve your location");
-        }
-        navigator.geolocation.getCurrentPosition(success, error);
-      }
-
-
-
-
-
-      function getApi(queryURL)
-
-      fetch(queryURL)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-
-
-
-
-
+  
+        
+  
+  
+    }
+  
+  
+  function getApi() {
+  
+     var queryURL = 'http://www.transitchicago.com/api/1.0/routes.aspx?outputType=JSON';
+  
+    fetch(queryURL) 
+        
+  
+    
+      .then(function (response) {
+        return response.json();
       })
-
+      .then(function (data) {
+        console.log(data);
+  
+      })
+  }
+  
+      submitButton.on('click', getApi);
+  
 
 
 
